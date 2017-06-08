44caeb3deef73216624d56ec323e6490e608498c
federico <federico@federico-HP-Notebook>
2017-06-08 22:04:03 +0200

    Struttura progetto

---
+++ galleria/src/main/java/it/uniroma3/galleria/SiwProgettoApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma3.galleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoApplication.class, args);
+	}
+}
