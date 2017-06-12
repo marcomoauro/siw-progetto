f9d4976a0224ec8960956acd5bfaa69de3538bdb
FedeR95 <federicoroma8@gmail.com>
2017-06-12 23:37:50 +0200

    Aggiunto login funzionante, con l'index fatta

--- galleria/src/main/java/it/uniroma3/galleria/controller/WebController.java
+++ galleria/src/main/java/it/uniroma3/galleria/controller/WebController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-	@RequestMapping(value={"/","/home"})
-	public String home(){
-		return "home";
-	}
+//	@RequestMapping(value={"/"})
+//	public String home(){
+//		return "home";
+//	}
 
 	@RequestMapping(value={"/welcome"})
 	public String welcome(){
