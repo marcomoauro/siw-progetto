56e9a58f78021888623230a0dbcaf91165b534e1
marcomoauro <marcomoauro@hotmail.it>
2017-06-12 22:28:52 +0200

    login

--- galleria/src/main/java/it/uniroma3/galleria/security/SecurityConfig.java
+++ galleria/src/main/java/it/uniroma3/galleria/security/SecurityConfig.java
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-//package it.uniroma3.galleria.security;
-// 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-// 
-//    @Autowired
-//    private DataSource dataSource;
-// 
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//		auth.jdbcAuthentication().dataSource(dataSource)
-//		
-//		.passwordEncoder(new BCryptPasswordEncoder())
-//		.usersByUsernameQuery("SELECT username,password,1 FROM users where username=?")
-//		.authoritiesByUsernameQuery("SELECT username,authority FROM authorities where username=?");
-//	}
-// 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http
-//        .authorizeRequests()
-//            .anyRequest().authenticated()
-//            .and()
-//        .formLogin()
-//            .loginPage("/login")
-//            .permitAll()
-//            .and()
-//        .logout()
-//           .permitAll();
-//    }
-//}
-=======
 package it.uniroma3.galleria.security;
  
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,7 +6,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import javax.sql.DataSource;
 
 @Configuration
@@ -61,28 +15,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
  
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+    @Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource)
-		.passwordEncoder(new BCryptPasswordEncoder())
-		.usersByUsernameQuery("SELECT username,password,1 FROM users where username=?")
-		.authoritiesByUsernameQuery("SELECT username,authority FROM authorities where username=?");
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
- 
+    
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-        .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-        .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-        .logout()
-           .permitAll();
-    }
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/home").permitAll().antMatchers("/admin").hasRole("ADMIN")
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
+				.permitAll();
+		http.exceptionHandling().accessDeniedPage("/403");
+	}
 }
->>>>>>> branch 'master' of https://github.com/FedeR95/siw-progetto.git
