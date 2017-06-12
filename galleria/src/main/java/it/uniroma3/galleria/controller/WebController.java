ba4bb33f1a875eee890ac15583ab44c4bec957c5
FedeR95 <federicoroma8@gmail.com>
2017-06-13 00:10:20 +0200

    prova css

--- galleria/src/main/java/it/uniroma3/galleria/controller/WebController.java
+++ galleria/src/main/java/it/uniroma3/galleria/controller/WebController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-//	@RequestMapping(value={"/"})
-//	public String home(){
-//		return "home";
-//	}
-
 	@RequestMapping(value={"/welcome"})
 	public String welcome(){
 		return "welcome";
