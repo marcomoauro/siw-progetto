ae6684e788406d4da282a97b40c939464bd89a38
marcomoauro <marcomoauro@hotmail.it>
2017-06-16 22:49:31 +0200

    modificata MostraDati dell'opera.

--- galleria/src/main/java/it/uniroma3/galleria/controller/AdminController.java
+++ galleria/src/main/java/it/uniroma3/galleria/controller/AdminController.java
@@ -8,7 +8,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import it.uniroma3.galleria.model.Ruolo;
 import it.uniroma3.galleria.model.Admin;
 import it.uniroma3.galleria.service.RuoloService;
