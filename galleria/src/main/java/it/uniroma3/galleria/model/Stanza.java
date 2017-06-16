0007ee3a960e7c4571446e4f11597993de1fc6fb
marcomoauro <marcomoauro@hotmail.it>
2017-06-16 10:00:01 +0200

    passaggio dal branch prova a master.

--- galleria/src/main/java/it/uniroma3/galleria/model/Stanza.java
+++ galleria/src/main/java/it/uniroma3/galleria/model/Stanza.java
@@ -2,13 +2,11 @@ package it.uniroma3.galleria.model;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
