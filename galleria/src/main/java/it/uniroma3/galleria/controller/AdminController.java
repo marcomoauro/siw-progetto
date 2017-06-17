672aa45d5e8de8be02c7de6d591449c3d2be44ca
marcomoauro <marcomoauro@hotmail.it>
2017-06-17 12:16:04 +0200

    aggiunta la possibilità di inserire nuovi admin.

--- galleria/src/main/java/it/uniroma3/galleria/controller/AdminController.java
+++ galleria/src/main/java/it/uniroma3/galleria/controller/AdminController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.galleria.controller;
 
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import it.uniroma3.galleria.model.Ruolo;
 import it.uniroma3.galleria.model.Admin;
 import it.uniroma3.galleria.service.RuoloService;
@@ -15,34 +17,63 @@ import it.uniroma3.galleria.service.AdminService;
 
 @Controller
 public class AdminController {
-	
+
 	@Autowired
 	private AdminService adminService;
-	
+
 	@Autowired
 	private RuoloService ruoloService;
-	
-	@GetMapping("admin/registraAdmin")
-	public String showForm(Admin admin){
-		return "signUp";
+
+	@GetMapping("admin/admin")
+	public String showForm(Model model){
+		List<Admin> admins = (List<Admin>) adminService.findAll();
+		model.addAttribute("admins",admins);
+		return "admin/admins";
 	}
-	
-	@PostMapping("admin/registraAdmin")
-	public String signUp(@Valid @ModelAttribute Admin admin, 
-			BindingResult bindingResult, Model model){
-		if (bindingResult.hasErrors()) {
-			return "signUp";
-		}else {
+
+	@GetMapping("admin/admin/inserisci")
+	public String adminForm(Admin admin){
+		return "admins/adminForm";
+
+	}
+
+	@PostMapping("admin/admin/inserisci")
+	public String checkAdminInfo(@Valid @ModelAttribute Admin admin, 
+			BindingResult result, Model model,@RequestParam("id") Long id) throws Exception{
+		if (result.hasErrors()) {
+			return "admins/adminForm";
+		}
+		else{
 			model.addAttribute(admin);
 			try{
-				Ruolo ruolo = new Ruolo(admin.getUsername());
-				adminService.add(admin); 
-				ruoloService.add(ruolo);
-			}catch(Exception e){
-				return "signUp";
+			Ruolo ruolo = new Ruolo(admin.getUsername());
+			admin.setEnabled(true);
+			adminService.add(admin);
+			ruoloService.add(ruolo);
+			}
+			catch(Exception e){
+				return "admins/adminForm";
 			}
 		}
-		return "datiAdmin";
+		return "admins/datiAdmin";
 	}
-	
+
+	//	@PostMapping("admin/registraAdmin")
+	//	public String signUp(@Valid @ModelAttribute Admin admin, 
+	//			BindingResult bindingResult, Model model){
+	//		if (bindingResult.hasErrors()) {
+	//			return "signUp";
+	//		}else {
+	//			model.addAttribute(admin);
+	//			try{
+	//				Ruolo ruolo = new Ruolo(admin.getUsername());
+	//				adminService.add(admin); 
+	//				ruoloService.add(ruolo);
+	//			}catch(Exception e){
+	//				return "signUp";
+	//			}
+	//		}
+	//		return "datiAdmin";
+	//	}
+
 }
